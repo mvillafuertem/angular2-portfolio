01ca863d3e5d101568775e256656bdb8b6bdbd17
mvillafuertem <mivimur@hotmail.com>
2017-04-15 13:33:04 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -31,6 +31,12 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
     reporters: config.angularCli && config.angularCli.codeCoverage
               ? ['progress', 'coverage-istanbul']
               : ['progress', 'kjhtml'],
@@ -42,3 +48,7 @@ module.exports = function (config) {
     singleRun: false
   });
 };
+
+if (process.env.TRAVIS) {
+    configuration.browsers = ['Chrome_travis_ci'];
+}
