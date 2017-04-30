0e388506eb0e59a52f79f3954ac6c7f0faf716b9
Miguel <mivimur@hotmail.com>
2017-05-01 00:43:40 +0200

    Updated .travis.yml

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-phantomjs-launcher')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -39,6 +40,9 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    if (process.env.TRAVIS) {
+      configuration.browsers = ['PhantomJS'];
+    }
   });
 };
